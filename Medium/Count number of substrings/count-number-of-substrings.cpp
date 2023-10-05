fa52f1af6182cb4d1d6f81e67fa1624bb03b73a1
chaitanya sikakolu <52370283+sikakoluchaitanya@users.noreply.github.com>
2023-10-05 23:17:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count number of substrings/count-number-of-substrings.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+   long atmost(string s, int k)
+    {
+        int arr[26] = {0};
+        int diff = 0; // to store number of different character
+        int left = 0; // left pointer which will get incremented if the there more different character then requiref
+        int result = 0;
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            arr[s[i] -'a']++;
+            if(arr[s[i]- 'a']==1)
+            {
+                diff++;
+            }
+            
+            while(diff>k)
+            {
+                arr[s[left]-'a']--;
+                
+                if(arr[s[left] -'a'] == 0)
+                {
+                    diff--;
+                }
+                
+                left++;
+            }
+            result = result + (i - left + 1);
+        }
+        return result;
+    }
+  
+    long long int substrCount (string s, int k) {
+    	
+    	return atmost(s,k) - atmost(s,k-1); 
+    }
+    
+    
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		string s; cin >> s;
+		int k; cin >> k;
+		Solution ob;
+		cout <<ob.substrCount (s, k) << endl;
+	}
+}
+// } Driver Code Ends
